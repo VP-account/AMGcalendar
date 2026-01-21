5fa6caa186007bf4564d95df9f7ba8cee966afb1
VP-account <vp.gorpynchuk@gmail.com>
2026-01-21 15:05:37 +0200

    Update prisma.ts

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,13 +1,12 @@
+// lib/prisma.ts
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+declare global {
+  var prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL,
-})
+export const prisma = globalThis.prisma || new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-
-export default prisma
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prisma = prisma
+}
